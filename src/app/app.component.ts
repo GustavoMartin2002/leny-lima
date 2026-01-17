645ac9bfbd387d07248838c9926aad504a2559e0
Gustavo <gustavolimamartin2002@gmail.com>
2026-01-17 16:44:22 -0300

    feat(app): Implementa Estrutura Base com Loading, Rotas e Suporte SSR
    - Estrutura principal com gerenciamento de estado (loading, router-outlet);
    - Implementa lógica de inicialização e monitoramento de rotas no controller;
    - Configura roteamento global e ativa scroll suave (anchor scrolling);
    - Estabelece provedores e rotas para Server-Side Rendering (SSR);
    - Atualiza teste unitário do componente raiz;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,23 @@
-import { ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { FooterComponent } from "./templates/footer/footer.component";
 import { NavComponent } from "./templates/nav/nav.component";
+import { FooterComponent } from "./templates/footer/footer.component";
 import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, FooterComponent, NavComponent],
+  imports: [CommonModule, RouterModule, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit{
-  title = 'myApp'
-  currentRoute = ''
 
-  constructor(private route: Router){}
+export class AppComponent implements OnInit {
+  title = 'Leny Lima';
+  loading: boolean = true;
+  currentRoute = '';
+  
+  constructor(private route: Router){};
 
   ngOnInit(): void {
     this.route.events
@@ -26,5 +25,6 @@ export class AppComponent implements OnInit{
       .subscribe((event: any) => {
         this.currentRoute = event.urlAfterRedirects;
       });
+    this.loading = false;
   }
 }
