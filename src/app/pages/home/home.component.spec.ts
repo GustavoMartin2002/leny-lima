82706b25a0a2e4b92c56623f8c27e45a3a141303
Gustavo <gustavolimamartin2002@gmail.com>
2026-01-21 15:32:18 -0300

    feat(pages): Reestrutura Home e Remove Módulo de Produtos
    - Refatora HomeComponent para arquitetura de seções (Início, Sobre, Mentorias, Histórico, Contato);
    - Remove ProductPage (funcionalidade será migrada para modais);
    - Ajusta testes unitários para nova estrutura de template;
    
    BREAKING CHANGE: A rota e o diretório da página de produtos foram removidos.

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +8,17 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: { title: '' },
+            },
+          },
+        },
+      ],
     })
     .compileComponents();
 
