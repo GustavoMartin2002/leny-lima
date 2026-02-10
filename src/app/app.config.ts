a4dece0e9f1366e25cf16d508d1ca72ea403a03e
Gustavo <gustavolimamartin2002@gmail.com>
2026-02-10 17:46:31 -0300

    perf(app): Ativa Hidratação com Event Replay e Fetch API
    - Adiciona `provideClientHydration` com `withEventReplay` para otimizar o TTI (Time to Interactive);
    - Habilita `withFetch` no `provideHttpClient` para requisições mais eficientes;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideRouter(
       routes,
       withInMemoryScrolling({
         anchorScrolling: 'enabled',
         scrollPositionRestoration: 'enabled',
-      })
-    )
+      }),
+    ),
+    provideClientHydration(withEventReplay())
   ]
 };
