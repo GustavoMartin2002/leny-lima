b00cecbede1c7b8441103b2f401e4ed96453e35d
Gustavo <gustavolimamartin2002@gmail.com>
2026-04-03 21:41:19 -0300

    feat(pages): Implementa Páginas de Política de Privacidade e Termos de Uso
    - Desenvolve componentes standalone para as rotas "/politica-de-privacidade" e "/termos-e-condicoes";
    - Implementa layouts estruturados e responsivos utilizando TailwindCSS;
    - Adiciona testes unitários (spec.ts) para garantir a integridade da criação dos componentes;

---
+++ src/app/pages/termos-e-condicoes/termos-e-condicoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termos-condicoes',
+  standalone: true,
+  templateUrl: './termos-e-condicoes.component.html',
+})
+export class TermosCondicoes {
+
+}
