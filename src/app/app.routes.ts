0d61e2c1d2310744ccc53e37a597091d6dbc59d2
Gustavo Martin <gustavolimamartin2002@gmail.com>
2026-08-11 21:44:23 -0300

    fix(deps): Corrige Vulnerabilidades (pnpm audit --fix=update)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent,
         pathMatch: 'full',
+        title: 'Leny Lima - Mentora de Autoconhecimento',
     },
     {
         path: 'inicio',
@@ -38,17 +39,21 @@ export const routes: Routes = [
     {
         path: 'politica-de-privacidade',
         component: PoliticaPrivacidade,
+        title: 'Política de Privacidade | Leny Lima',
     },
     {
         path: 'termos-e-condicoes',
         component: TermosCondicoes,
+        title: 'Termos e Condições | Leny Lima',
     },
     {
         path: 'florescer_start',
         component: FlorescerStartComponent,
+        title: 'Florescer Start | Leny Lima',
     },
     {
         path: '**',
         component: NotFoundComponent,
+        title: 'Página Não Encontrada | Leny Lima',
     },
 ];
